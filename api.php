93686d376f06f949b18c47ff0ce38a1fb37759af
Pau <pauiglesias@gmail.com>
2018-07-24 18:13:57 +0200

    Fixed the client class path.

--- api.php
+++ api.php
@@ -3,8 +3,8 @@
 // Load custom API configuration
 $config = include __DIR__.'/config.php';
 
-// Sooprema main API client class
-require_once __DIR__.'/src/sooprema/api-sdk/client.php';
+// Load Sooprema API client class
+require_once __DIR__.'/src/client.php';
 
 // API client instance
 $soopremaAPI = new \Sooprema\APISDK\Client($config);
